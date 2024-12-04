5a26723e88921524b36ed8c6c02e07d11fd337d3
JJUCIAL <sleep3rp@gmail.com>
2024-12-04 02:22:46 -0600

    2:22 2.3.2

--- assets/addressbar-color.ee117785.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./index.0bd244c3.js";var o=()=>{r.set("#a2e3fa")};export{o as default};
---
+++ assets/pinia.b5529a5c.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.247ef720.js";import"./index.f2ee513e.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
