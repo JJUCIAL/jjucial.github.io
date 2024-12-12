57b2909b6726d6860a25e2008699ae54193f4d17
JJUCIAL <sleep3rp@gmail.com>
2024-12-11 18:19:19 -0600

    table sortability push

---
+++ assets/pinia.87a19606.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.8273c1f7.js";import"./index.7cd608b3.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
--- assets/pinia.fd424991.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.2f1c16e1.js";import"./index.2e624e06.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
