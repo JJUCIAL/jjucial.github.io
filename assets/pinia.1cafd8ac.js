35bac7fc6b288d7faaf91bea3b344cf23b6d3bd7
JJUCIAL <sleep3rp@gmail.com>
2024-12-12 22:14:03 -0600

    Competition Table conditional rendering update

--- assets/pinia.1cafd8ac.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.271952ab.js";import"./index.7ed52cf1.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
---
+++ assets/pinia.7f930161.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.7b56fe7c.js";import"./index.42254923.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
