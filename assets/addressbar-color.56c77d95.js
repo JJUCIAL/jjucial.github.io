ab124d9ee3bcc27e95718327640260f4eb34f100
JJUCIAL <sleep3rp@gmail.com>
2025-02-19 13:59:01 -0600

    updated discord link - 19/02/25

---
+++ assets/addressbar-color.56c77d95.js
@@ -0,0 +1,1 @@
+import{A as o}from"./index.76458e0e.js";var a=()=>{console.log("color loaded"),o.set("#a2e3fa")};export{a as default};
--- assets/pinia.b87906fc.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.7e16700e.js";import"./index.d2256cf9.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
