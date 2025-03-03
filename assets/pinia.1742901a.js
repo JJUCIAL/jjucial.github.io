70fb99b7d039140c6e1cc2527a0cb0851d45bb07
JJUCIAL <sleep3rp@gmail.com>
2025-03-03 02:22:31 -0600

    q-slider added for bg-audio component 03/03/25

--- assets/pinia.1742901a.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.465ced4e.js";import"./index.db319cea.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
---
+++ assets/pinia.a2d89b7a.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.84efbeb3.js";import"./index.c5e3a542.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
