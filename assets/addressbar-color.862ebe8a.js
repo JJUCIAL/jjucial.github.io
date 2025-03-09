e7d4c803d2bf79efbd21a50ef57ac4a9ed8c7746
JJUCIAL <sleep3rp@gmail.com>
2025-03-08 18:42:56 -0600

    content push 08/03/25

--- assets/addressbar-color.862ebe8a.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index.9a45b20a.js";var a=()=>{console.log("color loaded"),o.set("#a2e3fa")};export{a as default};
--- assets/pinia.dd825c1d.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.2ea8aab1.js";import"./index.9a45b20a.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
