2db3745f79b269c61417d250d0b2a4068c6a6e7c
JJUCIAL <sleep3rp@gmail.com>
2025-06-09 20:32:55 -0500

    icon push

--- assets/addressbar-color.574868e5.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index.254b51ad.js";var a=()=>{console.log("color loaded"),o.set("#a2e3fa")};export{a as default};
---
+++ assets/pinia.8bc6d7a9.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.0c4fa5c0.js";import"./index.6067c68d.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
